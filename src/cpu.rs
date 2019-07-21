922c2322348a05646677cbb113c42c2046379ee6
Mark Zhang <ace119@163.com>
2019-07-21 10:40:48 +0800

    improve: Just save total & idle to calculate the cpu usage
    In order to calculate the cpu usage, we don't have to save all
    members in cpu stat, not to mention that these members are changing
    between different kernel versions(normally kernel just adds new
    members to not break userspace).
    
    To get the cpu usage, only 2 things are required: total & idle.
    So this patch changes the codes to just save these 2 things,
    which simplifies the codes and also make the codes work on differnt
    kernel versions.
    
    Signed-off-by: Mark Zhang <ace119@163.com>

--- src/cpu.rs
+++ src/cpu.rs
@@ -7,31 +7,15 @@ const CPU_STAT_FILE:&'static str = "/proc/stat";
 
 #[derive(PartialEq, Debug)]
 pub struct CpuStat {
-    user: usize,
-    nice: usize,
-    system: usize,
+    total: usize,
     idle: usize,
-    iowait: usize,
-    irq: usize,
-    softirq: usize,
-    stealstolen: usize,
-    guest: usize,
-    guest_nice: usize,
 }
 
 impl CpuStat {
     pub fn new() -> CpuStat {
         CpuStat {
-            user: 0,
-            nice: 0,
-            system: 0,
+            total: 0,
             idle: 0,
-            iowait: 0,
-            irq: 0,
-            softirq: 0,
-            stealstolen: 0,
-            guest: 0,
-            guest_nice: 0,
         }
     }
 }
@@ -92,46 +76,27 @@ pub fn collect_cpu_info(cpu_stats: &mut Vec<CpuStat>) -> Result<Vec<CpuInfoEntry
 
             if line.starts_with(name.as_str()) {
                 let v: Vec<&str> = line[(name.len() + 1)..].split(' ').collect();
-                let user = v[0].parse::<usize>().unwrap();
-                let nice = v[1].parse::<usize>().unwrap();
-                let system = v[2].parse::<usize>().unwrap();
-                let idle = v[3].parse::<usize>().unwrap();
-                let iowait = v[4].parse::<usize>().unwrap();
-                let irq = v[5].parse::<usize>().unwrap();
-                let softirq = v[6].parse::<usize>().unwrap();
-                let stealstolen = v[7].parse::<usize>().unwrap();
-                let guest = v[8].parse::<usize>().unwrap();
-                let mut guest_nice = 0;
-                if v.len() > 9 {
-                    guest_nice = v[9].parse::<usize>().unwrap();
+                let mut total = 0;
+                for i in 0..v.len() {
+                    total += v[i].parse::<usize>().unwrap();
                 }
+                // The 4th member is idle jiffies, kernel won't break userspace
+                let idle = v[3].parse::<usize>().unwrap();
 
                 cs = CpuStat {
-                    user: user,
-                    nice: nice,
-                    system: system,
+                    total: total,
                     idle: idle,
-                    iowait: iowait,
-                    irq: irq,
-                    softirq: softirq,
-                    stealstolen: stealstolen,
-                    guest: guest,
-                    guest_nice: guest_nice,
                 };
                 break;
             }
         }
 
-        if cs.user == 0 {
+        if cs.total == 0 {
             bail!(ErrorKind::CpuStatNotFound);
         }
         let new_stat = cs;
         let old_stat = &cpu_stats[i];
-        let total = (new_stat.user + new_stat.nice + new_stat.system + new_stat.idle +
-                    new_stat.iowait + new_stat.irq + new_stat.softirq + new_stat.stealstolen +
-                    new_stat.guest + new_stat.guest_nice) - (old_stat.user + old_stat.nice + old_stat.system +
-                    old_stat.idle + old_stat.iowait + old_stat.irq + old_stat.softirq +
-                    old_stat.stealstolen + old_stat.guest + old_stat.guest_nice);
+        let total = new_stat.total - old_stat.total;
         let idle = new_stat.idle - old_stat.idle;
         result[i].usage = 100.0 * (total - idle) as f64 / total as f64;
         cpu_stats[i] = new_stat;
@@ -176,37 +141,21 @@ pub fn initial_cpu_stats(cpu_num: usize) -> Result<Vec<CpuStat>> {
 
             if line.starts_with(name.as_str()) {
                 let v: Vec<&str> = line[(name.len() + 1)..].split(' ').collect();
-                let user = v[0].parse::<usize>().unwrap();
-                let nice = v[1].parse::<usize>().unwrap();
-                let system = v[2].parse::<usize>().unwrap();
-                let idle = v[3].parse::<usize>().unwrap();
-                let iowait = v[4].parse::<usize>().unwrap();
-                let irq = v[5].parse::<usize>().unwrap();
-                let softirq = v[6].parse::<usize>().unwrap();
-                let stealstolen = v[7].parse::<usize>().unwrap();
-                let guest = v[8].parse::<usize>().unwrap();
-                let mut guest_nice = 0;
-                if v.len() > 9 {
-                    guest_nice = v[9].parse::<usize>().unwrap();
+                let mut total = 0;
+                for i in 0..v.len() {
+                    total += v[i].parse::<usize>().unwrap();
                 }
+                let idle = v[3].parse::<usize>().unwrap();
 
                 cs = CpuStat {
-                    user: user,
-                    nice: nice,
-                    system: system,
+                    total: total,
                     idle: idle,
-                    iowait: iowait,
-                    irq: irq,
-                    softirq: softirq,
-                    stealstolen: stealstolen,
-                    guest: guest,
-                    guest_nice: guest_nice,
                 };
                 break;
             }
         }
 
-        if cs.user == 0 {
+        if cs.total == 0 {
             bail!(ErrorKind::CpuStatNotFound);
         } else {
             result.push(cs);
